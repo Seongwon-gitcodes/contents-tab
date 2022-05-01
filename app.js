2c6bd8cd2aa33cd36385ba5cd233281481d65229
seongwon-gitcodes <ysw991106@gmail.com>
2022-05-01 16:07:04 +0900

    데이터 불러오기

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ console.log(recentContents);
 
 const $listContent = document.getElementById('contents');
 
+let jsonData = recentContents;
+
+let count = 10;
+let arr = [];
+
+getData(jsonData);
+
 //탭 호출
 function callTab(event, tabName) {
     console.log("----------CALLTAB()----------");
@@ -28,5 +35,52 @@ function callTab(event, tabName) {
 
     // 호출 데이터 변경
     jsonData = eval(event.currentTarget.getAttribute("id") + "Contents");
+    getData(jsonData);
+}
 
+// 데이터 불러오기
+function getData(dataName) {
+    console.log("----------GETDATA()----------")
+    const item = JSON.parse(JSON.stringify(dataName));
+    for (key in item) {
+        const $id = item[key].id;
+        if (arr.includes($id)) {
+            continue;
+        } else {
+            arr.push($id);
+            const $img = document.createElement('img');
+            $img.setAttribute("id", "img");
+            $img.height = 200;
+            $img.width = 200;
+            $img.src = item[key].img;
+
+            const $title = document.createElement('div');
+            $title.setAttribute("id", "title");
+            $title.innerText = item[key].title;
+
+            const $url = document.createElement('a');
+            $url.setAttribute("id", "url");
+            $url.href = item[key].url;
+
+            const $cp = document.createElement('div');
+            $cp.setAttribute("id", "cp");
+            $cp.innerText = item[key].cp;
+
+            const $container = document.createElement('div');
+            $container.setAttribute("id","item");
+
+            $url.appendChild($img);
+            $url.appendChild($title);
+            $url.appendChild($cp);
+
+            $container.appendChild($url);
+
+            $listContent.appendChild($container);
+        }
+
+
+        if ($listContent.childElementCount === count) {
+            break;
+        }
+    }
 }
